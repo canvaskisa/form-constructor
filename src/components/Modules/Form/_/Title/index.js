983e002fb7420f84b53e260ffe8392fcf8a8ca03
canvaskisa <canvaskisa@gmail.com>
2016-08-25 12:42:50 +0300

    Update Aside and Form titles overflow.

--- src/components/Modules/Form/_/Title/index.js
+++ src/components/Modules/Form/_/Title/index.js
@@ -2,7 +2,9 @@ import React, {PropTypes as T} from 'react';
 import styles from './index.css';
 
 const Title = ({title}) => (
-	<h1 className={styles.root}>{title}</h1>
+	<h1 className={styles.root}>
+		<span className={styles.value}>{title}</span>
+	</h1>
 );
 
 Title.displayName = 'FormConstructor/Modules/Form/Title';
